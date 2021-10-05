71a6ecc63a71bf2dcfe7ed94fdc024317f23c205
Andrei Kuchynski <andrei_kuchynski1@epam.com>
2021-10-05 23:39:33 +0300

    add filter

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { MovieList } from "./Movie/MovieList";
+import { Filter } from "./Filter/Filter";
 
 export const Content = () => {
     return (
         <div className="content">
+            <Filter />
             <MovieList />
         </div>
     )
---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const Filter = () => {
+    return (
+        <div className="filter">
+            <div className="genre-filter">
+                <ul>
+                    <li><a className="active" href="#">ALL</a></li>
+                    <li><a href="#">DOCUMENTARY</a></li>
+                    <li><a href="#">COMEDY</a></li>
+                    <li><a href="#">HORROR</a></li>
+                    <li><a href="#">CRIME</a></li>
+                </ul>
+            </div>
+            <div class="date-filter">
+                <span className="sort">SORT BY</span>
+                <button className="drop-btn">RELEASE DATE</button>
+                <div className="dropdown-content">
+                    <a href="#">2021</a>
+                    <a href="#">2020</a>
+                    <a href="#">2019</a>
+                </div>
+            </div>
+        </div>
+    )
+}
