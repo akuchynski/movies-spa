e79b867057e331d0deee86b10960e61f9685d766
Andrei Kuchynski <andrei_kuchynski1@epam.com>
2021-11-06 15:57:27 +0300

    task6 fixes

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { getMoviesByDate, getMoviesByRating } from "../../store/thunks";
+import { getMovies, getMoviesByDate, getMoviesByRating } from "../../store/thunks";
 import { FilterLink } from './FilterLink';
 import { genres } from '../../utils/genres';
+import { filters } from '../../utils/filters';
 
 export const Filter = () => {
 
     const dispatch = useDispatch();
-    const loadByDate = () => dispatch(getMoviesByDate());
-    const loadByRating = () => dispatch(getMoviesByRating());
+    const [selectedOption, setSelectedOption] = useState(filters[0]);
+
+    const handleChange = (event) => {
+        setSelectedOption(event.target.value);
+        switch (event.target.value) {
+            case 'DATE':
+                return dispatch(getMoviesByDate());
+            case 'RATING':
+                return dispatch(getMoviesByRating());
+            default:
+                return dispatch(getMovies());
+        };
+    };
 
     return (
         <div className="filter">
@@ -19,11 +31,11 @@ export const Filter = () => {
             </div>
             <div className="date-filter">
                 <span className="sort">SORT BY</span>
-                <button className="drop-btn">RELEASE DATE</button>
-                <div className="dropdown-content">
-                    <a href="#" onClick={loadByDate}>DATE</a>
-                    <a href="#" onClick={loadByRating}>RATING</a>
-                </div>
+                <select value={selectedOption} onChange={handleChange}>
+                    {filters.map(item => (
+                        <option key={item} value={item}>{item}</option>
+                    ))}
+                </select>
             </div>
         </div>
     );
--- src/components/Movie/MovieList.js
+++ src/components/Movie/MovieList.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getMovies } from "../../store/thunks";
 import { MovieItem } from "./MovieItem";
+import { MovieMessages } from "./MovieMessages";
 
 export const MovieList = () => {
 
@@ -14,8 +15,7 @@ export const MovieList = () => {
 
     return (
         <div className="movie-list">
-            {isLoading && <h3>Loading movies...</h3>}
-            {errorMessage && <h3>{errorMessage}</h3>}
+            <MovieMessages isLoading={isLoading} errorMessage={errorMessage}/>
             {movies && movies.map(({ id, title, release_date, genres, poster_path }) => (
                 <MovieItem
                     key={id}
