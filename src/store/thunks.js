ad67b2249236290f6e30c886f3b333cb25dcd520
Andrei Kuchynski <andrei_kuchynski1@epam.com>
2021-11-10 18:34:41 +0300

    set base endpoint url

--- src/store/thunks.js
+++ src/store/thunks.js
@@ -1,44 +1,48 @@
 import axios from "axios";
 import actions from "./actions";
 
+const api = axios.create({
+    baseURL: 'http://localhost:4000/movies/'
+});
+
 export const getMovies = () => (dispatch) => {
     dispatch(actions.getMoviesRequest());
-    axios.get('http://localhost:4000/movies')
+    api.get('/')
         .then((response) => dispatch(actions.getMoviesSuccess(response.data)))
         .catch((error) => dispatch(actions.getMoviesError(error.message)));
 };
 
 export const getMoviesByGenre = (genre) => (dispatch) => {
     dispatch(actions.getMoviesRequest());
-    axios.get('http://localhost:4000/movies', genre !== 'All' && { params: { searchBy: 'genres', filter: genre } })
+    api.get('/', genre !== 'All' && { params: { searchBy: 'genres', filter: genre } })
         .then((response) => dispatch(actions.getMoviesSuccess(response.data)))
         .catch((error) => dispatch(actions.getMoviesError(error.message)));
 };
 
 export const getMoviesByDate = () => (dispatch) => {
     dispatch(actions.getMoviesRequest());
-    axios.get('http://localhost:4000/movies?sortBy=release_date&sortOrder=desc')
+    api.get('/', { params: { sortBy: 'release_date', sortOrder: 'desc' } })
         .then((response) => dispatch(actions.getMoviesSuccess(response.data)))
         .catch((error) => dispatch(actions.getMoviesError(error.message)));
 };
 
 export const getMoviesByRating = () => (dispatch) => {
     dispatch(actions.getMoviesRequest());
-    axios.get('http://localhost:4000/movies?sortBy=vote_average&sortOrder=desc')
+    api.get('/', { params: { sortBy: 'vote_average', sortOrder: 'desc' } })
         .then((response) => dispatch(actions.getMoviesSuccess(response.data)))
         .catch((error) => dispatch(actions.getMoviesError(error.message)));
 };
 
 export const getMovieById = (id) => (dispatch) => {
     dispatch(actions.getMovieRequest());
-    axios.get('http://localhost:4000/movies/' + id)
+    api.get('/' + id)
         .then((response) => dispatch(actions.getMovieSuccess(response.data)))
         .catch((error) => dispatch(actions.getMovieError(error.message)));
 };
 
 export const addMovie = (movie) => (dispatch) => {
     movie.genres = movie.genres.map(item => item.value);
-    axios.post('http://localhost:4000/movies', movie)
+    api.post('/', movie)
         .then((response) => dispatch(actions.addMovieSuccess(response.data)))
         .catch((error) => dispatch(actions.addMovieError(error.message)));
 };
@@ -46,13 +50,13 @@ export const addMovie = (movie) => (dispatch) => {
 export const updateMovie = (movieId, movie) => (dispatch) => {
     movie.id = movieId;
     movie.genres = movie.genres.map(item => item.value);
-    axios.put('http://localhost:4000/movies', movie)
+    api.put('/', movie)
         .then((response) => dispatch(actions.updateMovieSuccess(response.data)))
         .catch((error) => dispatch(actions.updateMovieError(error.message)));
 };
 
 export const deleteMovie = (id) => (dispatch) => {
-    axios.delete('http://localhost:4000/movies/' + id)
+    api.delete('/' + id)
         .then(() => dispatch(actions.deleteMovieSuccess()))
         .catch((error) => dispatch(actions.deleteMovieError(error.message)));
 };
